d322e8ea0f4cb743bb7915c68de2c6f69c50d78c
abhiac052 <49386428+abhiac052@users.noreply.github.com>
2022-10-05 08:10:08 +0530

    checking in again

--- src/redux/components/Todo.js
+++ src/redux/components/Todo.js
@@ -10,49 +10,39 @@ const Todo = () => {
 
   return (
     <>
-      <div className="container-fluid my-5">
-        <div className="row">
-          <div className="col-sm-3 mx-auto text-black shadow-lg p-3">
-            <h3>Todo App</h3>
-            <br />
-            <div className="input-group mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Add your new todo"
-                aria-describedby="button-addon2"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-              />
-              <button
-                className="btn btn-warning px-3 fw-bold"
-                type="button"
-                id="button-addon2"
-                onClick={() => dispatch(addTodo(input), setInput(""))}
-              >
-                +
-              </button>
-              <div className="container-fluid">
-                <ul className="list-unstyled row m-5" id="names">
-                  {list.map((comp) => {
-                    return (
-                      <div className="input-group mb-3" key={comp.id}>
-                        <h4>{comp.data}</h4>
-                        <button
-                          className="btn btn-danger"
-                          type="button"
-                          id="button-addon1"
-                          title="Delete"
-                          onClick={() => dispatch(deleteTodo(comp.id))}
-                        >
-                          <i class="bi bi-trash"></i>
-                        </button>
-                      </div>
-                    );
-                  })}
-                </ul>
+      <div className="container-fluid bg">
+        <div className = "row justify-content-center">
+          <div className="col-12 col-sm-6 col-md-3">
+
+            <div className="form-container">
+            <p class="fs-3 fw-bold">Todo App</p>
+              <div className="form-group">
+              <div className="input-group mb-3">
+              
+                <input type="text" className="form-control" placeholder="Add your new todo" aria-describedby="button-addon2"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                />
+                <button className="btn btn-warning px-3 fw-bold" type="button" id="button-addon2" onClick={() => dispatch(addTodo(input), setInput(""))}>
+                <span class="bi bi-plus" id="btn1"></span>
+                </button>
+                </div>
+                <div className="container-fluid">
+                  <ul className="list-unstyled row m-5" id="names">
+                    {list.map((comp) => {
+                      return (
+                        <div className="input-group mb-3" key={comp.id}>
+                          <h5>{comp.data}</h5>
+                          <button className="btn btn-danger" type="button" id="button-addon1" title="Delete" onClick={() => dispatch(deleteTodo(comp.id))}>
+                            <i class="bi bi-trash"></i>
+                          </button>
+                        </div>
+                        );
+                      })}
+                  </ul>
+                </div>
+                <button className = "btn btn-danger" id="btn2" type="button" title="Removeall" onClick={()=>dispatch(removeTodo())}>Clear All</button>
               </div>
-              <button className = "btn btn-danger" id="btn2" type="button" title="Removeall" onClick={()=>dispatch(removeTodo())}>Delete All</button>
             </div>
           </div>
         </div>
